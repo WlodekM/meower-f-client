fd9c34bde53b493af3d78991404f2e0818712673
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-03-14 18:27:46 +0200

    fix duplicate pfps when select hidden ones

--- src/screens/Profile.svelte
+++ src/screens/Profile.svelte
@@ -53,7 +53,8 @@
 	let pfpOverflow = false;
 	$: {
 		const pfp = $user.pfp_data;
-		pfpOverflow = pfp < 0 || pfp > PFP_COUNT;
+		let hiddenpfps = [-1, 500, 101, 102]
+		pfpOverflow = pfp < 0 || pfp > PFP_COUNT && !(hiddenpfps.includes(pfp));
 	}
 </script>
 
