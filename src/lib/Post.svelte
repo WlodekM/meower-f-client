b89e42e99563f242dafae467314f0c69f94ace39
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-03-15 17:59:50 +0200

    Update Post.svelte

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -340,7 +340,7 @@
 			<FormattedDate date={post.date} />
 		</div>
 	</div>
-	<p class="post-content">{convertLinks(post.content)}</p> <!-- ) doesnt work and all of <> convert to &lt;&gt; -->
+	<p class="post-content">{post.content}</p> <!-- all of the <tags> that convertLinks() makes get converted to &lt;tags&gt; -->
 	<div class="post-images">
 		{#each images as { title, url }}
 			<a href={url} target="_blank"
